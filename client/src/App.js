3f8105ca340669cc4a392913fa3334ae9f7d7cb8
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-02 15:58:12 -0700

    added router to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import { Home } from "./pages";
 import { Header, Footer } from "./components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
