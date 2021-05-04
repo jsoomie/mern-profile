f79448b4f4de400407d61d2a2901139f515608a3
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-04 10:48:12 -0700

    added links to the programs

--- client/src/components/ProgramList/ProgramList.js
+++ client/src/components/ProgramList/ProgramList.js
@@ -5,45 +5,56 @@ export const ProgramList = () => {
     {
       program: "MERN Stack",
       icon: "fas fa-layer-group",
+      link: "https://www.mongodb.com/mern-stack",
     },
     {
       program: "MongoDB",
       icon: "fas fa-database",
+      link: "https://www.mongodb.com/",
     },
     {
       program: "Express",
       icon: "fas fa-server",
+      link: "https://www.npmjs.com/package/express",
     },
     {
       program: "React.js",
       icon: "fab fa-react",
+      link: "https://reactjs.org/",
     },
     {
       program: "Node.js",
       icon: "fab fa-node",
+      link: "https://nodejs.org/en/about/",
     },
     {
       program: "JavaScript",
       icon: "fab fa-js",
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
     },
     {
       program: "HTML",
       icon: "fab fa-html5",
+      link: "https://developer.mozilla.org/en-US/docs/Glossary/HTML",
     },
     {
       program: "CSS",
       icon: "fab fa-css3-alt",
+      link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
     },
   ];
 
   return (
     <ul className="all-center program-list-container grid-4">
-      {programs.map(({ program, icon }) => (
-        <li key={program}>
-          <i className={icon} />
-          {" " + program}
-        </li>
-      ))}
+      {programs &&
+        programs.map(({ program, icon, link }) => (
+          <li key={program}>
+            <a href={link}>
+              <i className={icon} />
+              {" " + program}
+            </a>
+          </li>
+        ))}
     </ul>
   );
 };
