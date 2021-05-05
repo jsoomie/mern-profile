03377d26522a7121a9aeed072af8b6f64ea9a7ea
Jonathan Soukaseume <sksmejn@gmail.com>
2021-05-04 20:31:12 -0700

    added coloring to greeting page

--- client/src/components/Greeting/Greeting.js
+++ client/src/components/Greeting/Greeting.js
@@ -4,8 +4,12 @@ export const Greeting = () => {
   return (
     <div className="all-center greeting-container">
       <img src="./js_profile_hs.png" alt="my profile pic" width="300px" />
-      <h1 className="greeting-hello">Hello!</h1>
-      <h1>My name is Jonathan Soukaseume</h1>
+      <h1 className="greeting-hello">
+        Hello<span className="greeting-exclamation">!</span>
+      </h1>
+      <h1>
+        My name is <span className="greeting-name">Jonathan Soukaseume</span>
+      </h1>
       <div className="greeting-break">
         <hr />
       </div>
